8dd32d4fd50e53e0dc8b7ce3d519c6647b8846b1
Koushik R <rkoushik.14@gmail.com>
2025-02-20 15:07:13 +0530

    Formatting fixes + processorKey is now sent as one of the args to eventProcesor

--- concierge-core/src/main/java/com/grookage/concierge/core/managers/EventProcessor.java
+++ concierge-core/src/main/java/com/grookage/concierge/core/managers/EventProcessor.java
@@ -1,11 +1,12 @@
 package com.grookage.concierge.core.managers;
 
 import com.grookage.concierge.core.engine.ConciergeContext;
+import com.grookage.concierge.models.processor.ProcessorKey;
 
 public interface EventProcessor {
 
-    void preProcess(ConciergeContext context);
+    void preProcess(ProcessorKey processorKey, ConciergeContext context);
 
-    void postProcess(ConciergeContext context);
+    void postProcess(ProcessorKey processorKey, ConciergeContext context);
 
 }
